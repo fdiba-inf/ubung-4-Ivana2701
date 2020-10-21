68b38cc3186c99aa12f62f65aa88ef046dc9699c
Ivana2701 <ivanabaj@abv.bg>
2020-10-21 12:27:02 +0000

    .

---
+++ src/exercise4/ArrayOutput.java
@@ -0,0 +1,21 @@
+import java.Arrays;
+public class ArrayOutput {
+ 
+    public static void main(String[] args) {
+        Scanner input = new Scanne(System.in);
+        
+        int size = input.nextInt();
+        int[] numbers = new int[n];
+        
+        System.out.println("Enter the size of the array: ");
+        System.out.println("Enter the numbers: ");
+        for (int index = 0; index < numbers.length; index++) {
+            numbers[index] = input.nextInt();
+            for(int i = 0;)
+        }
+ 
+        String numbersAsString = Arrays.toString(numbers);
+        System.out.println("Numbers: " + numbersAsString);
+    }
+ 
+}
