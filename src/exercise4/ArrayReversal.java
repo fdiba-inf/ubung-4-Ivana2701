8e01eda2dc6a4ba81d977dc8dc52f62718f56411
Ivana2701 <ivanabaj@abv.bg>
2020-10-21 12:55:48 +0000

    .

---
+++ src/exercise4/ArrayReversal.java
@@ -0,0 +1,26 @@
+package exercise4;
+ 
+import java.util.Arrays;
+import java.util.Scanner;
+ 
+public class ArrayReversal {
+ 
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the size of the Array: ");
+        System.out.println("Enter text: ");
+        char[] array = new char[];
+        for(){
+
+            for(int i = array.length - 1; i >= 0; i--){
+            System.out.print(array[i] + " ");
+ 
+        }
+        System.out.print("Reversed: ");
+ 
+}
+
+ System.out.println(name1[1]);
+ 
+    }
+}
