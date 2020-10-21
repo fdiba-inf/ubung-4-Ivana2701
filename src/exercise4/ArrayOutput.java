267e8d8c6679130e807f657184ec240bb54d62fa
Ivana2701 <ivanabaj@abv.bg>
2020-10-21 14:59:49 +0000

    no

--- src/exercise4/ArrayOutput.java
+++ src/exercise4/ArrayOutput.java
@@ -1,17 +1,27 @@
-import java.Arrays;
+package exercise4;
+import java.util.Scanner;
+import java.util.Arrays;
 public class ArrayOutput {
  
     public static void main(String[] args) {
-        Scanner input = new Scanne(System.in);
-        
-        int size = input.nextInt();
-        int[] numbers = new int[n];
-        
+
+        Scanner input = new Scanner(System.in);   
+           
         System.out.println("Enter the size of the array: ");
+          int size = input.nextInt();
         System.out.println("Enter the numbers: ");
+         int[] numbers;
+         String space = " ";
+
         for (int index = 0; index < numbers.length; index++) {
+
             numbers[index] = input.nextInt();
-            for(int i = 0;)
+
+            for(int i = 0;i < numbers.length;i++){
+                System.out.println(numbers[index]);
+                System.out.print(space);
+                
+            }
         }
  
         String numbersAsString = Arrays.toString(numbers);
