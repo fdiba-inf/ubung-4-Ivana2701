78f68d007fd24d8c1b432d83a9bb2dcf20ff030f
Ivana2701 <ivanabaj@abv.bg>
2020-10-21 15:54:16 +0000

    HAH

--- src/exercise4/ArrayOutput.java
+++ src/exercise4/ArrayOutput.java
@@ -10,18 +10,21 @@ public class ArrayOutput {
         System.out.println("Enter the size of the array: ");
           int size = input.nextInt();
         System.out.println("Enter the numbers: ");
-         int[] numbers;
+         int[] numbers = new int[size];
          String space = " ";
+
+         for (int index = 0; index < numbers.length; index++) {
+
+            numbers[index] = input.nextInt();
+      }
 
         for (int index = 0; index < numbers.length; index++) {
 
-            numbers[index] = input.nextInt();
-
-            for(int i = 0;i < numbers.length;i++){
-                System.out.println(numbers[index]);
+            for(int i = 0;i < index;i++){
                 System.out.print(space);
-                
             }
+
+                System.out.println(numbers[index]);
         }
  
         String numbersAsString = Arrays.toString(numbers);
