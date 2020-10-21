88c44c397a531ef63e901388b26ac25950de9ebd
Ivana2701 <ivanabaj@abv.bg>
2020-10-21 15:06:53 +0000

    /

---
+++ src/exercise4/MinIndex.java
@@ -0,0 +1,17 @@
+package exercise4;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayInputOutput {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+              System.out.print("Enter the value for the array: ");
+              int size = input.nextInt();
+              System.out.println("Enter the array numbers: ");
+              int numbers[] = new int[size];
+    }
+
+}
